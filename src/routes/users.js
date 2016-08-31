0f6b57c10fb67660f643e70466e7d1cf095ccc00
zhudm <zhudm@chetong.net>
2016-08-31 20:33:11 +0800

    调整登录验证语

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,20 +8,29 @@ router.post('/login', function(req, res, next) {
   var name = req.body.name;
   var pwd = req.body.pwd;
   var sql = 'select * from users where name = "' + name + '" and passwd = "' + pwd + '"';
-  util.query(sql).then(function(result){
-  	if(result.length > 0){
+  util.query(sql).then(function(result) {
+    if (result.length > 0) {
       req.session.userInfo = {
-        userId : result[0].id,
-        userName : result[0].name,
-        userClass : result[0].class
+        userId: result[0].id,
+        userName: result[0].name,
+        userClass: result[0].class
       };
-  		res.end(JSON.stringify({resultCode:"000",userId:result[0].id}));
-  	}else{
-  		res.end(JSON.stringify({resultCode:"999",msg:"用户不存在!"}));
-  	}
-  },function(){
-  	res.end(JSON.stringify({resultCode:"999",msg:"服务器异常，请稍后再试！"}));
-  })
+      res.end(JSON.stringify({
+        resultCode: "000",
+        userId: result[0].id
+      }));
+    } else {
+      res.end(JSON.stringify({
+        resultCode: "999",
+        msg: "账号或密码错误！"
+      }));
+    }
+  }, function() {
+    res.end(JSON.stringify({
+      resultCode: "999",
+      msg: "服务器异常，请稍后再试！"
+    }));
+  });
 
 });
 
@@ -29,29 +38,43 @@ router.post('/login', function(req, res, next) {
 router.post('/register', function(req, res, next) {
   var body = req.body;
   var name = body.name;
-  if(name.length < 1 || name.length > 10){
-    res.end(JSON.stringify({resultCode:"999",msg:"账号长度不符！"}));
+  if (name.length < 1 || name.length > 10) {
+    res.end(JSON.stringify({
+      resultCode: "999",
+      msg: "账号长度不符！"
+    }));
     return;
   }
   var pwd = body.pwd;
-  if(!/^[a-zA-Z0-9_!@#$%^&*()-\\+]{6,15}$/.test(pwd)){
-    res.end(JSON.stringify({resultCode:"999",msg:"密码不符规范！"}));
+  if (!/^[a-zA-Z0-9_!@#$%^&*()-\\+]{6,15}$/.test(pwd)) {
+    res.end(JSON.stringify({
+      resultCode: "999",
+      msg: "密码不符规范！"
+    }));
     return;
   }
   var pwd2 = body.confirm_pwd;
-  if(pwd != pwd2){
-    res.end(JSON.stringify({resultCode:"999",msg:"两次输入的密码不一致！"}));
+  if (pwd != pwd2) {
+    res.end(JSON.stringify({
+      resultCode: "999",
+      msg: "两次输入的密码不一致！"
+    }));
     return;
   }
   var sql = 'insert into users(name,passwd,date) values ("' + name + '", "' + pwd + '",' + new Date().getTime() + ')';
-  util.insert(sql).then(function(result){
-    res.end(JSON.stringify({resultCode:"000",msg:"注册成功！"}));
-  },function(err){
-    res.end(JSON.stringify({resultCode:"999",msg:"服务器异常，请稍后再试！"}));
-  })
+  util.insert(sql).then(function(result) {
+    res.end(JSON.stringify({
+      resultCode: "000",
+      msg: "注册成功！"
+    }));
+  }, function(err) {
+    res.end(JSON.stringify({
+      resultCode: "999",
+      msg: "服务器异常，请稍后再试！"
+    }));
+  });
 
 });
-
 
 
 
