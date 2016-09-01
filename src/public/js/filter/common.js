a2d627ee13daa7741281091f110cd92a6f8fa6a7
zhudm <zhudm@chetong.net>
2016-09-01 20:56:32 +0800

    样式加filter

---
+++ src/public/js/filter/common.js
@@ -0,0 +1,9 @@
+angular.module("lichu").filter("titleFilter", [function() {
+	return function(input) {
+		if (input.title.trim() === "") {
+			return input.date;
+		} else {
+			return input.title;
+		}
+	};
+}]);
