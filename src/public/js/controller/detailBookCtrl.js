e8d1b945249d95182ed41fee4700ff3841a60ee0
zhudm <601443405@qq.com>
2016-09-09 22:46:08 +0800

    文本可以换行，以及空格。

--- src/public/js/controller/detailBookCtrl.js
+++ src/public/js/controller/detailBookCtrl.js
@@ -1,10 +1,15 @@
 angular.module("lichu").controller('detailBookCtrl', ['$scope', '$stateParams', 'apis', 'popupService',function($scope, $stateParams, apis, popupService) {
 	var id = $stateParams.bookId;
 
+	var formatText = function(text){
+		return text.replace(/\n/gm,"<br/>").replace(/\s/gm,"&nbsp");
+	};
+
 	apis.getDetailBookbyId.send({
 		id: id
 	}, null).then(function(result) {
 		if (result.resultCode == "000") {
+			result.resultObject.content = formatText(result.resultObject.content);
 			$scope.item = result.resultObject;
 		} else {
 			popupService.fail(result.msg);
