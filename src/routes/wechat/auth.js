f64522c7563e5dce09e83ec0cf15217086308422
zhudm <601443405@qq.com>
2017-04-18 21:51:48 +0800

    修复微信token验证

--- src/routes/wechat/auth.js
+++ src/routes/wechat/auth.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require("crypto");
+var config = require("../../config.json");
 
 /* GET home page. */
 router.get('/auth', function(req, res, next) {
-	var token = req.query.token,
+	var signature = req.query.signature,
 		timestamp = req.query.timestamp,
 		nonce = req.query.nonce,
 		echostr = req.query.echostr;
-		console.log(echostr+"---"+ timestamp+"---"+  nonce+"---"+  token)
-	if (isLegel(echostr, timestamp, nonce, token)) {
+		console.log("token=" + config.wechat.token);
+		console.log(echostr+"---"+ timestamp+"---"+  nonce+"---"+  signature)
+	if (isLegel(signature, timestamp, nonce, config.wechat.token)) {
 		res.send(echostr);
 	} else {
 		res.send("失败");
